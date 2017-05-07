b24cca29372c27bece1e615000c544caf1ca5aa2
otacke <o.tacke@web.de>
2017-05-07 20:42:44 +0200

    fix fullscreen on iOS

--- scripts/imageSlider.js
+++ scripts/imageSlider.js
@@ -101,20 +101,38 @@
       var maximumWidth, maximumHeight;
       var targetWidth, targetHeight;
 
-      // Get maximum width and height that can be displayed.
+      /*
+      * Get maximum width and height that can be displayed.
+      * Unfortunately, the iPhone requires a special treatment as it doesn't
+      * work with the measuring the container, and it also requires individual
+      * treatment for landscape and portrait orientation.
+      */
       if (H5P.isFullscreen) {
-        maximumWidth = parseInt(window.getComputedStyle(this.parent.container).width);
-        maximumHeight = parseInt(window.getComputedStyle(this.parent.container).height);
+        if (isMobileDevice() && (/iPhone/.test(navigator.userAgent))) {
+          if (Math.abs(window.orientation) === 90) {
+            maximumWidth = screen.height;
+            maximumHeight = screen.width;
+          }
+          else {
+            maximumWidth = screen.availWidth;
+            maximumHeight = screen.availHeight;
+          }
+        }
+        else {
+          maximumWidth = parseInt(window.getComputedStyle(this.parent.container).width);
+          maximumHeight = parseInt(window.getComputedStyle(this.parent.container).height);
+        }
       }
       else {
+        // Not full screen.
         maximumWidth = Math.min(this.options.maximumWidth, parseInt(document.querySelector(this.selector).offsetWidth));
         maximumHeight = this.options.maximumHeight;
       }
 
-      // Scale to width or to height.
+      // Scale to width or to height, whatever leads to a bigger image.
       var maxRatio = maximumWidth / maximumHeight;
       if (maxRatio <= this.imageRatio) {
-        targetWidth = Math.min(Math.floor(window.innerWidth - 2), maximumWidth);
+        targetWidth = Math.min(Math.floor(window.innerWidth), maximumWidth);
         targetHeight = Math.round(targetWidth / this.imageRatio);
       }
       else {
@@ -124,16 +142,19 @@
 
       // Add passepartout - we don't need one at the top/bottom if not on fullscreen.
       if (H5P.isFullscreen) {
-        this.wrapper.style.paddingTop = Math.floor((parseInt(window.getComputedStyle(this.parent.container).height) - targetHeight) / 2) + 'px ';
+        this.wrapper.style.paddingTop = Math.floor((maximumHeight - targetHeight) / 2) + 'px ';
         this.wrapper.style.paddingBottom = this.wrapper.style.paddingTop;
+        this.wrapper.style.paddingLeft = Math.floor((maximumWidth - targetWidth) / 2) + 'px';
+        this.wrapper.style.paddingRight = this.wrapper.style.paddingLeft;
       } else {
         this.wrapper.style.paddingTop = 0;
         this.wrapper.style.paddingBottom = 0;
+        this.wrapper.style.paddingLeft = Math.floor((window.innerWidth - targetWidth) / 2) + 'px';
+        this.wrapper.style.paddingRight = this.wrapper.style.paddingLeft;
       }
-      this.wrapper.style.paddingLeft = Math.floor((window.innerWidth - 2 - targetWidth) / 2) + 'px';
-      this.wrapper.style.paddingRight = this.wrapper.style.paddingLeft;
 
       this.wrapper.style.height = targetHeight + 'px';
+
       // The InternetExplorer needs this explicit width and height for images
       this.imgBefore.image.setAttribute('width', targetWidth);
       this.imgBefore.image.setAttribute('height', targetHeight);
@@ -455,6 +476,18 @@
       x: pageX,
       y: pageY
     };
+  };
+
+  /**
+   * Detect mobile devices (http://detectmobilebrowsers.com/)
+   *
+   * @returns {boolean} True if running on a mobile device.
+   */
+  var isMobileDevice = function() {
+    var check = false;
+    (function(a){
+      if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;}) (navigator.userAgent||navigator.vendor||window.opera);
+    return check;
   };
 
 })(H5P.ImageJuxtaposition);
